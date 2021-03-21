7260856fe287d529fd3440d7f174875dd73bac62
Esha-Khalid <eshaKhalid46@gmail.com>
2021-03-21 23:10:24 +0500

    While Loop

---
+++ while loop (3).cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=1;
+	int sum=0;
+	while(a<=5)
+	{
+		cout<<a<<endl;
+      sum=sum+a;
+      a=a+1;
+	}
+    	cout<<"sum:"<<sum<<endl;
+}
